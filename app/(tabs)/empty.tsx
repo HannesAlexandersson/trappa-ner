b4020f5b656d8288f5e863cdab43555c0ee59cb3
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2026-04-15 14:43:37 +0200

    Adding tabs routes
    Adding the tab routes.
    Adding style to tabs layout
    Adding modal support for panic button.
    Adding Support for web UI

--- app/(tabs)/empty.tsx
+++ app/(tabs)/empty.tsx
@@ -1,4 +1,4 @@
 //we want the tab to remain on the tab bar but we want the actual route to be modal so this is empty and points to the modal
-export default function () {
+export default function Empty() {
   return null;
 }
