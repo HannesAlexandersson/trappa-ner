c5eea571e658c3f361eb19cfcaaa71b4dce046ff
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2026-05-22 11:58:01 +0200

    Update zodSchemas.ts

--- utils/zodSchemas.ts
+++ utils/zodSchemas.ts
@@ -3,5 +3,4 @@ import { z } from "zod";
 export const loginSchema = z.object({
   emailAddress: z.string().email("Invalid email format"),
   password: z.string().min(8, "Password must be at least 8 characters long"),
-  errors: z.string(),
 });
