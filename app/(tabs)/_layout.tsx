1ea3a96790ae93a2f02b434fe8fdc1b109673a0e
Hannes Alexandersson <S103154@gmail.com>
2026-08-20 18:45:09 +0200

    Adding back btn prevention
    The user can no not back to far back so they back out of the app

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import { View } from "react-native";
 
 export default function TabLayout() {
+
   return (
     <Tabs
       screenOptions={{
@@ -17,18 +18,6 @@ export default function TabLayout() {
           fontWeight: "bold",
           fontFamily: "Roboto",
         },
-        /*  headerBackground: () => (
-          <ImageBackground
-            source={require("@/assets/images/image.png")}
-            style={{
-              width: "100%",
-              height: "100%",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-            resizeMode="cover"
-          />
-        ), */
       }}
     >
       <Tabs.Screen
