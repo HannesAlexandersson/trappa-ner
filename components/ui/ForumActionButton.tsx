78045f617c0f4c313852b6021951acd6ff649df3
Hannes Alexandersson <S103154@gmail.com>
2026-09-24 12:51:30 +0200

    Adding the reply-to-thread logic

--- components/ui/ForumActionButton.tsx
+++ components/ui/ForumActionButton.tsx
@@ -21,7 +21,7 @@ const ForumActionButton = ({
     return (
         <Pressable
             onPress={onPress}
-            className="flex-row items-center bg-vgrBlue dark:bg-white rounded-full px-4 py-2"
+            className="flex-row items-center bg-vgrBlue dark:bg-white rounded-full px-4 py-2 active:scale-75"
         >
 
             <Ionicons
